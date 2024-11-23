49ecd4005df00cee4bd119e78f4bb8864f9c3f20
Sureshkumar T <tsureshkumar2000@gmail.com>
2024-11-24 04:34:00 +0530

    Solve problem 1140

---
+++ src/1140.cpp
@@ -0,0 +1,142 @@
+/*
+ * Author: Sureshkumar T <tsureshkumar2000@gmail.com>
+ * Copyright (C) 2024 Sureshkumar T. All rights reserved.
+ */
+
+
+// https://cses.fi/problemset/task/1140
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <bitset>
+#include <unordered_set>
+#include <random>
+#include <functional>
+#include <iterator>
+
+using namespace std;
+
+//----------TEMPLATES----------
+template<typename... T>
+void see(T&... args) { ((cin >> args), ...);}
+template<typename... T>
+void put(T&&... args) { ((cout << args << " "), ...);}
+template<typename... T>
+void putl(T&&... args) { ((cout << args << " "), ...); cout<<'\n';}
+#define error(...) { string _s = #__VA_ARGS__; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, __VA_ARGS__); }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {cerr << *it << "=" << a << ", "; err(++it, args...);}
+
+//----------MACROS----------
+#define LL                  long long
+#define INF                 ((1e9))
+#define EPS                 (0.0000001)
+#define PI                  (acos(-1.0))
+#define MD                  (1000000007)
+#define ii                  pair<int,int>
+#define ll                  pair<LL,LL>
+#define vii                 vector<pair<int,int> >
+#define vi                  vector<int>
+#define vvi                 vector<vector<int> >
+#define vvii                vector<vector<ii> >
+#define vl                  vector<long long>
+#define vvl                 vector<vector<long long> >
+#define rep(i,n)            for(int i=0, _##i=(n); i<_##i; ++i)
+#define dwn(i,n)            for(int i=(n); --i>=0; )
+#define ntimes(n)           while(n-- >0)
+#define repr(i,l,r)         for(int i=(l), _##i=(r); i<_##i; ++i)
+#define dwnr(i,l,r)         for(int i=(r), _##i=(l); --i>=_##i; )
+#define repi(i,a)           for(__typeof((a).begin()) i=(a).begin(), _##i=(a).end(); i!=_##i; ++i)
+#define dwni(i,a)           for(__typeof((a).rbegin()) i=(a).rbegin(), _##i=(a).rend(); i!=_##i; ++i)
+#define BitLSOne(x)         ((x)&(-x))
+#define BitLSOneRst(x)      ((x)&(x-1))
+#define inbound(x,l,r)      (l < x && x < r)
+#define inboundi(x,l,r)     (l <= x && x <= r)
+#define overlaps(x,y,a,b)   ((min(y,b)-max(x,a)) > 0)
+#define overlapwin(x,y,a,b) ((min(y,b)-max(x,a)))
+#define all(a)              (a).begin(), (a).end()
+#define bitcount            __builtin_popcount
+#define gcd                 __gcd
+#define INDEX(arr,ind)      (lower_bound(all(arr),ind)-arr.begin())
+#define sz                  size()
+#define seea(a,x,y)         for(int i=x;i<y;i++){cin>>a[i];}
+#define seev(v,n)           for(int i=0;i<n;i++){int x; cin>>x; v.push_back(x);}
+#define putv(v,n,k)         { for(int i=0;i<n;i++){ setw(k); cout << v[i]; } cout << "\n"; }
+#define sees(s,n)           for(int i=0;i<n;i++){int x; cin>>x; s.insert(x);}
+#define pb                  push_back
+#define fi                  first
+#define se                  second
+#define setpr(x)            cout<<setprecision(x)<<fixed
+#define setw(x)             cout<<setw(x)<<fixed
+
+/*debug functions*/
+#if DEBUG == 1
+#define HIGHLIGHT "\u001b[33m"
+#define RESETHIGH "\u001b[00m"
+struct debugger{template<typename T> debugger& operator ,(const T& v){std::cout<<v<<" ";return *this;}}dbg;
+#define debug(...) {cout << HIGHLIGHT << #__VA_ARGS__ << ": "; dbg,__VA_ARGS__;std::cout<< RESETHIGH << std::endl;}
+#define debugv(v) {cout << HIGHLIGHT; for(__typeof(v[0]) &x:v) { std::cout << x << " "; } cout << std::endl; cout << RESETHIGH;}
+#else
+#define debug(...) // Just strip off all debug tokens
+#define debugv(...) // Just strip off all debug tokens
+#define DEBUG 0
+#endif
+
+int main(int argc, char **argv) {
+    cin.tie(0)->sync_with_stdio(false);
+
+    int n; see(n);
+    int a,b,p;
+    vector<tuple<int,int,int>> v;
+    rep(i,n) {
+        see(a,b,p);
+        v.pb({a,b,p});
+    }
+
+    // sort by end time
+    sort(all(v), [](tuple<int,int,int> a, tuple<int,int,int> b) {
+        return get<1>(a) <= get<1>(b);
+    });
+
+    vector<tuple<int,int,int>> v2;
+    for(auto [a,b,p] : v) {
+        if(!v2.empty() && get<1>(v2.back()) == b && get<0>(v2.back()) == a && get<2>(v2.back()) <= p) {
+            v2.pop_back();
+        }
+        v2.pb({a,b,p});
+    }
+    swap(v, v2);
+
+
+    vl best(n);
+    for(int i=0; i<n; i++) {
+        auto [a,b,p] = v[i];
+
+        auto it = upper_bound(all(v), tuple<int,int,int>{0,a,0}, 
+            [](tuple<int,int,int> a, tuple<int,int,int> b) {
+            return get<1>(a) <= get<1>(b);
+        });
+        int j = it - v.begin() - 1;
+        if(j >= 0) {
+            best[i] = max(best[i-1], (LL)p + best[j]);
+        } else {
+            best[i] = max(i>0 ?best[i-1] : 0, (LL)p);
+        }
+        debug(i, j, a,b,p, best[i]);
+    }
+
+    putl(best[n-1]);
+
+
+    return 0;
+}
