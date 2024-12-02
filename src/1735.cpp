8683d1ca255adac2f677df51192e6d71b17bb212
Sureshkumar T <tsureshkumar2000@gmail.com>
2024-12-02 12:12:00 +0530

    Solve issue #1735: Range Updates and Sums - Implemented efficient solution for updating ranges and calculating sums.

---
+++ src/1735.cpp
@@ -0,0 +1,185 @@
+/*
+ * Author: Sureshkumar T <tsureshkumar2000@gmail.com>
+ * Copyright (C) 2024 Sureshkumar T. All rights reserved.
+ */
+
+
+// https://cses.fi/problemset/task/1735
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <bitset>
+#include <unordered_set>
+#include <random>
+#include <functional>
+#include <iterator>
+
+using namespace std;
+
+//----------TEMPLATES----------
+template<typename... T>
+void see(T&... args) { ((cin >> args), ...);}
+template<typename... T>
+void put(T&&... args) { ((cout << args << " "), ...);}
+template<typename... T>
+void putl(T&&... args) { ((cout << args << " "), ...); cout<<"\n";}
+#define error(...) { string _s = #__VA_ARGS__; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s);istream_iterator<string> _it(_ss); err(_it, __VA_ARGS__); }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {cerr << *it << "=" << a << ", "; err(++it, args...);}
+
+//----------MACROS----------
+#define LL                  long long
+#define INF                 ((1e9))
+#define EPS                 (0.0000001)
+#define PI                  (acos(-1.0))
+#define MD                  (1000000007)
+#define ii                  pair<int,int>
+#define ll                  pair<LL,LL>
+#define vii                 vector<pair<int,int> >
+#define vi                  vector<int>
+#define vvi                 vector<vector<int> >
+#define vvii                vector<vector<ii> >
+#define vl                  vector<long long>
+#define vvl                 vector<vector<long long> >
+#define rep(i,n)            for(int i=0, _##i=(n); i<_##i; ++i)
+#define dwn(i,n)            for(int i=(n); --i>=0; )
+#define ntimes(n)           while(n-- >0)
+#define repr(i,l,r)         for(int i=(l), _##i=(r); i<_##i; ++i)
+#define dwnr(i,l,r)         for(int i=(r), _##i=(l); --i>=_##i; )
+#define repi(i,a)           for(__typeof((a).begin()) i=(a).begin(), _##i=(a).end(); i!=_##i; ++i)
+#define dwni(i,a)           for(__typeof((a).rbegin()) i=(a).rbegin(), _##i=(a).rend(); i!=_##i; ++i)
+#define BitLSOne(x)         ((x)&(-x))
+#define BitLSOneRst(x)      ((x)&(x-1))
+#define inbound(x,l,r)      (l < x && x < r)
+#define inboundi(x,l,r)     (l <= x && x <= r)
+#define overlaps(x,y,a,b)   ((min(y,b)-max(x,a)) > 0)
+#define overlapwin(x,y,a,b) ((min(y,b)-max(x,a)))
+#define all(a)              (a).begin(), (a).end()
+#define bitcount            __builtin_popcount
+#define gcd                 __gcd
+#define INDEX(arr,ind)      (lower_bound(all(arr),ind)-arr.begin())
+#define sz                  size()
+#define seea(a,x,y)         for(int i=x;i<y;i++){cin>>a[i];}
+#define seev(v,n)           for(int i=0;i<n;i++){int x; cin>>x; v.push_back(x);}
+#define putv(v,n,k)         { for(int i=0;i<n;i++){ setw(k); cout << v[i]; } cout << ""; }
+#define sees(s,n)           for(int i=0;i<n;i++){int x; cin>>x; s.insert(x);}
+#define pb                  push_back
+#define fi                  first
+#define se                  second
+#define setpr(x)            cout<<setprecision(x)<<fixed
+#define setw(x)             cout<<setw(x)<<fixed
+
+/*// debug functions*/
+#if DEBUG == 1
+#define HIGHLIGHT "\u001b[33m"
+#define RESETHIGH "\u001b[00m"
+struct debugger{template<typename T> debugger& operator ,(const T& v){std::cout<<v<<" ";return *this;}}dbg;
+#define debug(...) {cout << HIGHLIGHT << #__VA_ARGS__ << ": "; dbg,__VA_ARGS__;std::cout<< RESETHIGH << std::endl;}
+#define debugv(v) {cout << HIGHLIGHT; for(__typeof(v[0]) &x:v) { std::cout << x << " "; } cout << std::endl; cout << RESETHIGH;}
+#else
+#define debug(...) // Just strip off all debug tokens
+#define debugv(...) // Just strip off all debug tokens
+#define DEBUG 0
+#endif
+
+struct LazySegmentTree {
+    // lazy is always for the child nodes and for propagation
+    vl tree, lazy;
+    vi hardset; // hardset[v] = 1 if the node has been set, 0 otherwise
+    LazySegmentTree (int n) {
+        tree.resize (4*n+1, 0);
+        lazy.resize (4*n+1, 0);
+        hardset.resize (4*n+1, 0);
+    }
+    void push (int v, int tl, int tr) {
+        if (hardset[v] == 1) {
+            tree[v] = lazy[v] * (tr-tl+1); // then apply if any pending lazy that happened after hardset
+            if (tl != tr) {
+                hardset[v*2] = hardset[v];
+                hardset[v*2+1] = hardset[v];
+                lazy[v*2] = lazy[v];
+                lazy[v*2+1] = lazy[v];
+            }
+            lazy[v] = 0;
+            hardset[v] = 0;
+            return;
+        }
+        if (lazy[v] != 0) {
+            tree[v] += lazy[v] * (tr-tl+1);
+            if (tl != tr) {
+                lazy[v*2] += lazy[v];
+                lazy[v*2+1] += lazy[v];
+            }
+            lazy[v] = 0;
+        }
+    }
+    void update (int v, int tl, int tr, int l, int r, int add, bool isHardset = false) {
+        push (v, tl, tr);
+        if (tr < l || tl > r) return;
+        if (l <= tl && tr <= r) {
+            lazy[v] += add;
+            if (isHardset) {
+                hardset[v] = 1;
+                lazy[v] = add;
+            }
+            push (v, tl, tr);
+            return;
+        }
+        int tm = (tl + tr) / 2;
+        update (v*2, tl, tm, l, min(r, tm), add, isHardset);
+        update (v*2+1, tm+1, tr, max(l, tm+1), r, add, isHardset);
+        tree[v] = tree[v*2] + tree[v*2+1];
+    }
+    LL query (int v, int tl, int tr, int l, int r) {
+        push (v, tl, tr);
+
+        if (tr < l || tl > r) return 0;
+
+        if (l <= tl && tr <= r) return tree[v];
+        int tm = (tl + tr) / 2;
+        return query (v*2, tl, tm, l, min(r, tm)) + query (v*2+1, tm+1, tr, max(l, tm+1), r);
+    }
+    void set (int v, int tl, int tr, int l, int r, int x) {
+        update (v, tl, tr, l, r, x, true);
+    }
+};
+
+int main(int argc, char **argv) {
+    cin.tie(0)->sync_with_stdio(false);
+
+    int n, q; see(n, q);
+    vi v; seev(v, n);
+
+    int N = 1<<(32-__builtin_clz(n));
+    LazySegmentTree st(N);
+    rep (i, n) st.update (1, 0, N-1, i, i, v[i]);
+
+    rep (_, q) {
+        int t; see(t);
+        if (t == 1) { // increase by x for the range [a,b]
+            int a, b, x; see(a, b, x);
+            // debug("update", a, b, x);
+            st.update (1, 0, N-1, a-1, b-1, x);
+        } else if (t == 3) { // query the range [a,b]
+            int a, b; see(a, b);
+            putl (st.query (1, 0, N-1, a-1, b-1));
+        } else { // set to x in the range [a,b]
+            int a, b, x; see(a, b, x);
+            //debug("set", a, b, x);
+            st.set (1, 0, N-1, a-1, b-1, x);
+        }
+    }
+
+    return 0;
+}
