1f5928f57750a1dcae49a4100518cef5ae6b0ef8
Sureshkumar T <tsureshkumar2000@gmail.com>
2024-12-04 23:28:00 +0530

    Solve the Minimal Rotation problem (1110): implement a function that rotates an array of integers by a given number of positions to minimize the sum of absolute differences between adjacent elements.

---
+++ src/1110.cpp
@@ -0,0 +1,141 @@
+/*
+ * Author: Sureshkumar T <tsureshkumar2000@gmail.com>
+ * Copyright (C) 2024 Sureshkumar T. All rights reserved.
+ */
+
+
+// https://cses.fi/problemset/task/1110
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <bitset>
+#include <unordered_set>
+#include <random>
+#include <functional>
+#include <iterator>
+
+using namespace std;
+
+//----------TEMPLATES----------
+template<typename... T>
+void see(T&... args) { ((cin >> args), ...);}
+template<typename... T>
+void put(T&&... args) { ((cout << args << " "), ...);}
+template<typename... T>
+void putl(T&&... args) { ((cout << args << " "), ...); cout<<"\n";}
+#define error(...) { string _s = #__VA_ARGS__; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s);istream_iterator<string> _it(_ss); err(_it, __VA_ARGS__); }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {cerr << *it << "=" << a << ", "; err(++it, args...);}
+
+//----------MACROS----------
+#define LL                  long long
+#define INF                 ((1e9))
+#define EPS                 (0.0000001)
+#define PI                  (acos(-1.0))
+#define MD                  (1000000007)
+#define ii                  pair<int,int>
+#define ll                  pair<LL,LL>
+#define vii                 vector<pair<int,int> >
+#define vi                  vector<int>
+#define vvi                 vector<vector<int> >
+#define vvii                vector<vector<ii> >
+#define vl                  vector<long long>
+#define vvl                 vector<vector<long long> >
+#define rep(i,n)            for(int i=0, _##i=(n); i<_##i; ++i)
+#define dwn(i,n)            for(int i=(n); --i>=0; )
+#define ntimes(n)           while(n-- >0)
+#define repr(i,l,r)         for(int i=(l), _##i=(r); i<_##i; ++i)
+#define dwnr(i,l,r)         for(int i=(r), _##i=(l); --i>=_##i; )
+#define repi(i,a)           for(__typeof((a).begin()) i=(a).begin(), _##i=(a).end(); i!=_##i; ++i)
+#define dwni(i,a)           for(__typeof((a).rbegin()) i=(a).rbegin(), _##i=(a).rend(); i!=_##i; ++i)
+#define BitLSOne(x)         ((x)&(-x))
+#define BitLSOneRst(x)      ((x)&(x-1))
+#define inbound(x,l,r)      (l < x && x < r)
+#define inboundi(x,l,r)     (l <= x && x <= r)
+#define overlaps(x,y,a,b)   ((min(y,b)-max(x,a)) > 0)
+#define overlapwin(x,y,a,b) ((min(y,b)-max(x,a)))
+#define all(a)              (a).begin(), (a).end()
+#define bitcount            __builtin_popcount
+#define gcd                 __gcd
+#define INDEX(arr,ind)      (lower_bound(all(arr),ind)-arr.begin())
+#define sz                  size()
+#define seea(a,x,y)         for(int i=x;i<y;i++){cin>>a[i];}
+#define seev(v,n)           for(int i=0;i<n;i++){int x; cin>>x; v.push_back(x);}
+#define putv(v,n,k)         { for(int i=0;i<n;i++){ setw(k); cout << v[i]; } cout << ""; }
+#define sees(s,n)           for(int i=0;i<n;i++){int x; cin>>x; s.insert(x);}
+#define pb                  push_back
+#define fi                  first
+#define se                  second
+#define setpr(x)            cout<<setprecision(x)<<fixed
+#define setw(x)             cout<<setw(x)<<fixed
+
+/*debug functions*/
+#if DEBUG == 1
+#define HIGHLIGHT "\u001b[33m"
+#define RESETHIGH "\u001b[00m"
+struct debugger{template<typename T> debugger& operator ,(const T& v){std::cout<<v<<" ";return *this;}}dbg;
+#define debug(...) {cout << HIGHLIGHT << #__VA_ARGS__ << ": "; dbg,__VA_ARGS__;std::cout<< RESETHIGH << std::endl;}
+#define debugv(v) {cout << HIGHLIGHT; for(__typeof(v[0]) &x:v) { std::cout << x << " "; } cout << std::endl; cout << RESETHIGH;}
+#else
+#define debug(...) // Just strip off all debug tokens
+#define debugv(...) // Just strip off all debug tokens
+#define DEBUG 0
+#endif
+
+vi cnt, tmp;
+vi tempFa;
+
+   int main(int argc, char **argv) {
+      cin.tie(0)->sync_with_stdio(false);
+ 
+      string s; see(s);
+      int n = s.size();
+      string t = s + s;
+      int m = t.size();
+      debug(n);
+ 
+      vi hashes(m+1), pows(m+1) ;
+      const int A = 31, B= (int)1e9+9;
+      hashes[0] = t[0]-'a', pows[0] = 1;
+      rep(i,m) {
+        hashes[i+1] = ((LL)hashes[i] * A + t[i+1]-'a') % B;
+        pows[i+1] = (LL)pows[i] * A % B;
+      }
+
+      auto getHash = [&](int l, int r) {
+        // res will have A^l * a_l + A^(l-1) * a_l+1 + ... + A^0 * a_r
+        return (LL) (hashes[r] - (l > 0 ? (LL)hashes[l-1] * pows[r-l+1] % B : 0) + B) % B;
+      };
+
+      int smallest =  0;
+      for(int j=1; j<n; j++) {
+        debug(j, j+n-1, smallest, getHash(j, j+n-1), getHash(smallest, smallest+n-1));
+        int l=j, r=j+n-1, mid;
+        while(l <= r) {
+          mid = l + (r-l)/2;
+          debug(l, mid, r, getHash(l,mid), getHash(smallest + l-j, smallest+(mid-j)));
+
+          if(getHash(l,mid) == getHash(smallest + l-j, smallest+(mid-j)))
+            l = mid+1;
+          else
+            r = mid-1;
+        }
+        debug(l, j+n, t[l], t[smallest+l-j]);
+        if(l < j+n && t[l] < t[smallest+l-j]) smallest = j;
+      }
+      debug(smallest);
+      putl(t.substr(smallest, n));
+
+      return 0;
+  }
